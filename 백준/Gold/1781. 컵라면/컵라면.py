1e88b79f3fdcdb9438b8ba4886e206200fe471c4
column-wise <61263933+column-wise@users.noreply.github.com>
2024-04-29 21:18:48 +0900

    [Gold II] Title: 컵라면, Time: 628 ms, Memory: 61720 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+
+read = sys.stdin.readline
+
+N = int(read().strip("\n"))
+
+problems = []
+
+for _ in range(N):
+    deadline, cup_noodle = map(int, read().strip("\n").split())
+    problems.append((deadline, cup_noodle))
+
+problems.sort()
+
+queue = []
+
+for deadline, cup_noodle in problems:
+    heapq.heappush(queue, cup_noodle)
+    if deadline < len(queue):
+        heapq.heappop(queue)
+
+print(sum(queue))
