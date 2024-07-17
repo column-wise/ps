b9c2adc84f1cdf2d0687fcc0821d5cbffc05ee80
column-wise <61263933+column-wise@users.noreply.github.com>
2024-07-17 20:59:31 +0900

    [Silver II] Title: 마지막 팩토리얼 수, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static long answer;
+
+	public static void main(String[] args) throws Exception {
+		SetData();
+		System.out.println(answer%10);
+	}
+
+	private static void SetData() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long input = Long.parseLong(br.readLine());
+		answer =1;
+		
+		for(int i=1;i<=input;i++) {
+			answer*=i;
+			answer %= 1000000000;
+			while (answer % 10 == 0)
+			     answer /= 10;
+		}
+	}
+}
