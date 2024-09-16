e10c210b79c78e4f74c7f613cb64d5131c910d0e
Juhyun Lee <61263933+column-wise@users.noreply.github.com>
2024-09-16 15:38:25 +0900

    [Silver I] Title: n단 논법, Time: 96 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/15723. n단 논법/n단 논법.java
+++ 백준/Silver/15723. n단 논법/n단 논법.java
@@ -8,10 +8,18 @@ public class Main {
         StringTokenizer st = null;
 
         int n = Integer.parseInt(br.readLine());
+        
+        // list directChild 에는 바로 자식만 담을거고
+        // set childs에는 손자 손녀 다 담을 예정
         List<List<Integer>> directChild = new ArrayList<>();
+        HashSet<Integer>[] childs = new HashSet[26];
 
         for(int i = 0; i < 26; i++){
             directChild.add(new ArrayList<>());
+            childs[i] = new HashSet<Integer>();
+            
+            // a is a, z is z 처리를 위해 자기 자신은 미리 담아주었음
+            childs[i].add(i);
         }
 
         for(int i = 0; i < n; i++){
@@ -23,26 +31,15 @@ public class Main {
             directChild.get(from).add(to);
         }
 
-        int m = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int from = st.nextToken().charAt(0) - 'a';
-            st.nextToken();
-            int to = st.nextToken().charAt(0) - 'a';
-
+        for(int i = 0; i < 26; i++){
             Deque<Integer> queue = new ArrayDeque<>();
             boolean[] visited = new boolean[26];
-            queue.add(from);
-            visited[from] = true;
-            boolean isRight = false;
+            queue.add(i);
+            visited[i] = true;
 
             while(!queue.isEmpty()){
                 int cur = queue.poll();
-                if(cur == to){
-                    isRight = true;
-                    break;
-                }
+                childs[i].add(cur);
 
                 for(int child : directChild.get(cur)){
                     if(!visited[child]){
@@ -52,7 +49,17 @@ public class Main {
                 }
             }
 
-            if(isRight){
+        }
+
+        int m = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = st.nextToken().charAt(0) - 'a';
+            st.nextToken();
+            int to = st.nextToken().charAt(0) - 'a';
+
+            if(childs[from].contains(to)){
                 System.out.println("T");
             } else{
                 System.out.println("F");
