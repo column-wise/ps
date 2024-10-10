ba0d27ae8c3c5978153e55eb3b6d59beeeada501
Juhyun Lee <61263933+column-wise@users.noreply.github.com>
2024-10-10 17:49:34 +0900

    [D4] Title: [Professional] 키 순서, Time: 921 ms, Memory: 92,436 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,80 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int N;
-    static int[][] adjMatrix;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        for(int test_case = 1; test_case <= T; test_case++){
-            N = Integer.parseInt(br.readLine());
-            int M = Integer.parseInt(br.readLine());
-
-            int ans = 0;
-
-            adjMatrix = new int[N][N];
-
-            for(int i = 0; i < M; i++){
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken()) - 1;
-                int b = Integer.parseInt(st.nextToken()) - 1;
-                adjMatrix[a][b] = 1;
-            }
-
-            for(int i = 0; i < N; i++){
-                if(gtBfs(i) + ltBfs(i) == N-1){
-                    ans++;
-                }
-            }
-
-            System.out.println("#"+test_case+" "+ans);
-        }
-
-    }
-
-    private static int gtBfs(int start){
-        int cnt = 0;
-        Deque<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[N];
-
-        queue.add(start);
-        visited[start] = true;
-
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            for(int i = 0; i < N; i++){
-                if(!visited[i] && adjMatrix[cur][i] != 0){
-                    queue.offer(i);
-                    visited[i] = true;
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-
-    private static int ltBfs(int start){
-        int cnt = 0;
-        Deque<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[N];
-
-        queue.add(start);
-        visited[start] = true;
-
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            for(int i = 0; i < N; i++){
-                if(!visited[i] && adjMatrix[i][cur] != 0){
-                    queue.offer(i);
-                    visited[i] = true;
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-
+	static final int INF = 10000;
+	static int N;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+			
+			int[][] adjMatrix = new int[N][N];
+			
+			for(int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken()) - 1;
+				int b = Integer.parseInt(st.nextToken()) - 1;
+				
+				adjMatrix[a][b] = 1;
+			}
+			
+			int[][] dist = new int[N][N];
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(i == j) continue;
+					if(adjMatrix[i][j] == 0) {
+						dist[i][j] = INF;
+					} else {
+						dist[i][j] = 1;
+					}
+				}
+			}
+			
+			floydWarshall(adjMatrix, dist);
+			
+			int result = 0;
+			
+			for(int i = 0; i < N; i++) {
+				boolean[] canDetermineHeight = new boolean[N];
+				int count = 0;
+				
+				for(int j = 0; j < N; j++) {
+					if(dist[i][j] != INF) canDetermineHeight[j] = true;
+				}
+				
+				for(int j = 0; j < N; j++) {
+					if(dist[j][i] != INF) canDetermineHeight[j] = true;
+				}
+				
+				for(int j = 0; j < N; j++) {
+					if(canDetermineHeight[j]) count++;
+				}
+				
+				if(count == N) result++;
+			}
+			
+			System.out.println("#"+test_case+" "+result);
+		}
+	}
+	
+	private static void floydWarshall(int[][] adjMatrix, int[][] dist) {
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+				}
+			}
+		}
+	}
 }
