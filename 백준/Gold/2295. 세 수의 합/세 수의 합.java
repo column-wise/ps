11463aae002b90a2d2534c42db2c7407492c6733
Juhyun Lee <61263933+column-wise@users.noreply.github.com>
2024-10-12 19:35:28 +0900

    [Gold IV] Title: 세 수의 합, Time: 248 ms, Memory: 22448 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -7,36 +7,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] seq = new int[N];
+        int[] sumOfTwoNums = new int[((N) * (N+1))/2];
 
         for(int i = 0; i < N; i++){
             seq[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(seq);
 
+        int idx=0;
+        for(int i = 0; i < N; i++){
+            for(int j = i; j < N; j++){
+                sumOfTwoNums[idx++] = seq[i]+seq[j];
+            }
+        }
+        Arrays.sort(sumOfTwoNums);
+
+        // a + b + c = d를 만족하는 (a+b) 값이 sumOfTwoNums에 있는지 탐색
         for(int i = N-1; i >= 0; i--){
-            int target = seq[i];
+            int d = seq[i];
             for(int j = i-1; j >= 0; j--){
-                int b = seq[j];
-                for(int k = j; k >= 0; k--){
-                    int c = seq[k];
+                int c = seq[j];
 
-                    int start = 0;
-                    int end = k;
-                    int mid;
+                int start = 0;
+                int end = sumOfTwoNums.length - 1;
+                int mid;
 
-                    while(start<=end){
-                        mid = (start+end) / 2;
-                        int a = seq[mid];
-
-                        if(a + b + c < target){
-                            start = mid+1;
-                        } else if (a + b + c > target) {
-                            end = mid-1;
-                        } else {
-                            System.out.println(target);
-                            return;
-                        }
+                while(start<=end){
+                    mid = (start + end) / 2;
+                    if(sumOfTwoNums[mid] + c < d){
+                        start = mid + 1;
+                    } else if(sumOfTwoNums[mid] + c > d){
+                        end = mid - 1;
+                    } else {
+                        System.out.println(d);
+                        return;
                     }
                 }
             }
