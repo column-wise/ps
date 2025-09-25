23e66f1ee8e7549d4ee9723c2b8bd53d96926a97
Juhyun Lee <columnwise99@gmail.com>
2025-09-25 15:04:43 +0900

    [Gold IV] Title: DSLR, Time: 2852 ms, Memory: 299960 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,80 +1,91 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        for(int test_case = 1; test_case <= T; test_case++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
+	static char[] inst = {'D', 'S', 'L', 'R'};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder answer = new StringBuilder();
+		StringTokenizer st;
+		Queue<Node> queue;
+		int T = Integer.parseInt(br.readLine());
 
-            Deque<Node> queue = new ArrayDeque<>();
-            queue.add(new Node(A, ""));
+		for(int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
 
-            boolean[] visited = new boolean[10000];
-            visited[A] = true;
+			boolean[] visited = new boolean[10000];
+			int[] prev = new int[10000];
+			char[] how = new char[10000];
 
-            while(!queue.isEmpty()) {
-                Node node = queue.poll();
+			queue = new ArrayDeque<>();
+			visited[from] = true;
+			prev[from] = -1;
+			queue.add(new Node(from));
 
-                if(node.num == B){
-                    sb.append(node.commandList);
-                    sb.append("\n");
-                    break;
-                } else{
-                    int currentNum = node.num;
-                    String currentCommandList = node.commandList;
+			while(!queue.isEmpty()) {
+				Node node = queue.poll();
+				int value = node.register;
 
-                    int newNum = currentNum * 2 % 10000;
-                    if(!visited[newNum]){
-                        queue.add(new Node(newNum, currentCommandList+"D"));
-                        visited[newNum] = true;
-                    }
+				if(value == to) break;
 
-                    newNum = currentNum - 1;
-                    if(newNum == -1){
-                        newNum = 9999;
-                    }
-                    if(!visited[newNum]){
-                        queue.add(new Node(newNum, currentCommandList+"S"));
-                        visited[newNum] = true;
-                    }
+				for(int i = 0; i < 4; i++) {
+					int next = operation(value, i);
+					if(!visited[next]) {
+						visited[next] = true;
+						prev[next] = value;
+						how[next] = inst[i];
+						queue.add(new Node(next));
+					}
+				}
+			}
 
-                    newNum = currentNum * 10;
-                    int temp = newNum/10000;
-                    newNum = newNum%10000;
-                    newNum += temp;
+			int cur = to;
+			StringBuilder route = new StringBuilder();
+			while(prev[cur] != -1) {
+				route.append(how[cur]);
+				cur = prev[cur];
+			}
 
-                    if(!visited[newNum]) {
-                        queue.add(new Node(newNum, currentCommandList+"L"));
-                        visited[newNum] = true;
-                    }
+			answer.append(route.reverse()).append("\n");
+		}
 
-                    temp = currentNum%10;
-                    newNum = currentNum/10;
-                    newNum += temp*1000;
-                    if(!visited[newNum]) {
-                        queue.add(new Node(newNum, currentCommandList+"R"));
-                        visited[newNum] = true;
-                    }
-                }
-            }
-        }
-        System.out.println(sb);
-    }
+		System.out.println(answer);
+	}
 
-    private static class Node {
-        int num;
-        String commandList;
+	private static class Node {
+		int register;
 
-        private Node(int num, String commandList) {
-            this.num = num;
-            this.commandList = commandList;
-        }
-    }
+		public Node(int value) {
+			register = value;
+		}
+	}
+
+	private static int operation(int value, int i) {
+		if(i == 0) return D(value);
+		if(i == 1) return S(value);
+		if(i == 2) return L(value);
+		if(i == 3) return R(value);
+		return -1;
+	}
+
+	private static int D(int value) {
+		return value * 2 % 10000;
+	}
+
+	private static int S(int value) {
+		return (value + 9999) % 10000;
+	}
+
+	private static int L(int value) {
+		return (value % 1000) * 10 + value / 1000;
+	}
+
+	private static int R(int value) {
+		return (value % 10) * 1000 + value / 10;
+	}
 }
