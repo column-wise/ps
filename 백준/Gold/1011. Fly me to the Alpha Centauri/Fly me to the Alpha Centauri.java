2e9cff2fb2739d507b50e508cf05a8f5d3a2796b
Juhyun Lee <columnwise99@gmail.com>
2025-09-25 14:13:23 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 96 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -1,37 +1,32 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= T; test_case++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+		for(int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
 
-            int distance = to - from;
+			int distance = to - from;
+			int root = (int) Math.sqrt(distance);
 
-            int section = 1;
-            boolean isSectionUpdateNeeded = false;
-            int times = 0;
+			if(distance == root * root) {
+				sb.append(2*root - 1);
+			} else if(distance <= root * root + root) {
+				sb.append(2*root);
+			} else {
+				sb.append(2*root + 1);
+			}
 
-            while(distance > 0) {
-                distance -= section;
-                times++;
-                if(isSectionUpdateNeeded){
-                    section++;
-                    isSectionUpdateNeeded = false;
-                } else{
-                    isSectionUpdateNeeded = true;
-                }
-            }
-
-            System.out.println(times);
-        }
-    }
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
 }
