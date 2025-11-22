1ab2c24beb0fca0afe7c0c13a3af3d6d64c29ab5
Juhyun Lee <columnwise99@gmail.com>
2025-11-22 16:40:20 +0900

    [Gold IV] Title: 단어 수학, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,70 +1,35 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-	static char[] alphabets = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','V','X','Y','Z'};
-	static int alphabetNum = 26;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		List<Alphabet> list = new ArrayList<>();
-		for(int i = 0; i < alphabets.length; i++) {
-			list.add(new Alphabet(alphabets[i], 0));
-		}
-		
-		for(int i = 0; i < N; i++) {
-			String[] str = br.readLine().split("");
-			int s = str.length;
-			for(int j = s-1; j >= 0; j--) {
-				int n = (int)Math.pow(10, s-1-j);
-				
-				for(int k = 0; k < alphabetNum; k++) {
-					if(list.get(k).a == str[j].charAt(0)) {
-						list.get(k).n += n;
-						break;
-					}
-				}
-			}
-			
-		}
-		
-		Collections.sort(list);
-		
-		int i = 0;
-		int multiplier = 9;
-		int sum = 0;
-		while(list.get(i).n != 0) {
-			sum += list.get(i).n * multiplier;
-			i++;
-			multiplier--;
-		}
-		
-		System.out.println(sum);
-		
-	}
-	
-	public static class Alphabet implements Comparable<Alphabet>{
-		char a;
-		int n;
-		
-		public Alphabet(char a, int n) {
-			this.a = a;
-			this.n = n;
-		}
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Map<Character, Integer> map = new HashMap<>();
+        int answer = 0;
 
-		@Override
-		public String toString() {
-			return "Alphabet [a=" + a + ", n=" + n + "]";
-		}
+        for(int i = 0; i < N; i++) {
+            char[] inputs = br.readLine().toCharArray();
+            int base = 1;
+            for(int j = inputs.length - 1; j >= 0; j--) {
+                map.put(inputs[j], map.getOrDefault(inputs[j], 0) + base);
+                base *= 10;
+            }
+        }
 
-		@Override
-		public int compareTo(Alphabet o) {
-			return -(n-o.n);
-		}
-	}
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> o2.getValue() - o1.getValue());
+
+        int n = 9;
+        for(Map.Entry<Character, Integer> entry : list) {
+            answer += entry.getValue() * n;
+            n--;
+        }
+
+        System.out.println(answer);
+    }
 }
